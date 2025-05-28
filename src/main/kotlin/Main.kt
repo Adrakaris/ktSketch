cc04f88ef01d5d97eb32780addd76f5b723e30d5
Yijun <me@yijun.hu>
2025-05-28 02:09:39 +0100

    Theme change animation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 package hu.yijun
 
-import com.formdev.flatlaf.FlatLightLaf
+import com.formdev.flatlaf.FlatDarkLaf
 import com.formdev.flatlaf.extras.FlatInspector
 import com.formdev.flatlaf.extras.FlatUIDefaultsInspector
+import com.formdev.flatlaf.util.UIScale
 import hu.yijun.theme.Theme
 import hu.yijun.theme.ThemeManager
 import hu.yijun.view.Sketchbook
@@ -11,17 +12,16 @@ import javax.swing.SwingUtilities
 import javax.swing.WindowConstants.EXIT_ON_CLOSE
 
 fun main() {
-
     FlatInspector.install("ctrl shift alt X")
     FlatUIDefaultsInspector.install("ctrl shift alt Y")
 
     SwingUtilities.invokeLater {
-        FlatLightLaf.setup()
-        ThemeManager.setTheme(Theme.LIGHT)
+        FlatDarkLaf.setup()
+        ThemeManager.setTheme(Theme.DARK)
 
         val window = JFrame("KtSketchbook")
         window.defaultCloseOperation = EXIT_ON_CLOSE
-        window.setSize(1280, 960)
+        window.setSize(UIScale.scale(1280), UIScale.scale(960))
 
         val sketchbook = Sketchbook()
         window.add(sketchbook)
--- src/main/kotlin/theme/ThemeManager.kt
+++ src/main/kotlin/theme/ThemeManager.kt
@@ -2,6 +2,7 @@ package hu.yijun.theme
 
 import com.formdev.flatlaf.FlatDarkLaf
 import com.formdev.flatlaf.FlatLightLaf
+import com.formdev.flatlaf.extras.FlatAnimatedLafChange
 import java.awt.Font
 import javax.swing.JWindow
 import javax.swing.SwingUtilities
@@ -15,7 +16,7 @@ object ThemeManager {
     private val lightLaf = FlatLightLaf()
     private val darkLaf = FlatDarkLaf()
 
-    private var currentTheme: Theme = Theme.LIGHT
+    private var currentTheme: Theme = Theme.DARK
     private var currentFont: Font? = null
 
     fun setTheme(theme: Theme) {
@@ -23,6 +24,7 @@ object ThemeManager {
 
         currentTheme = theme
 
+        FlatAnimatedLafChange.showSnapshot()
         UIManager.setLookAndFeel(
             when (theme) {
                 Theme.LIGHT -> lightLaf
@@ -31,6 +33,7 @@ object ThemeManager {
         )
 
         updateFrameUI()
+        FlatAnimatedLafChange.hideSnapshotWithAnimation()
     }
 
     fun theme() = currentTheme
